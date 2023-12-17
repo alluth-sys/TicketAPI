1805b7dceea14a4f9f76882a17954dd370d6230f
Albert Lu <lualbert88@gmail.com>
2023-12-17 18:18:17 +0800

    Add project files.

---
+++ Models/Concert.cs
@@ -0,0 +1,17 @@
+﻿namespace TicketAPI.Models
+{
+    public class Concert
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime PlayingDate {get; set;}
+        public string[] PaymentMethods { get; set;}
+        public string Notice { get; set;}
+        public string[] Channel {  get; set; }
+
+
+
+
+    }
+}
