92b7fcd1926ed36a3f6f73de6552b08c6753f704
Albert Lu <lualbert88@gmail.com>
2024-01-01 16:59:35 +0800

    feat: add JWT config

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TicketAPI.CloudStorage;
 
 namespace TicketAPI
@@ -8,6 +11,7 @@ namespace TicketAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var Configuration = builder.Configuration;
 
             // Add services to the container.
 
@@ -16,6 +20,32 @@ namespace TicketAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: "LocalHostOrigins",
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:3000")
+                                             .AllowAnyHeader()
+                                             .AllowAnyMethod();
+                                  });
+            });
 
             var app = builder.Build();
 
@@ -27,9 +57,9 @@ namespace TicketAPI
             }
 
             app.UseHttpsRedirection();
-
+            app.UseCors("LocalHostOrigins");
             app.UseAuthorization();
-
+            app.UseAuthentication();
 
             app.MapControllers();
 
